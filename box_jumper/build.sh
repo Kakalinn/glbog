34a25f618f4ee09ab367e5d3319fa4dc9211aa4e
Bergur Snorrason <bsnorrason@gmail.com>
2021-08-11 16:45:22 +0000

    increased the res on the build script.

--- box_jumper/build.sh
+++ box_jumper/build.sh
@@ -1,1 +1,1 @@
-gcc box_jumper.c $(pkg-config --libs --cflags opengl glfw3 glew) -g -lm -DMOUSE_MOVEMENT $@
+gcc box_jumper.c $(pkg-config --libs --cflags opengl glfw3 glew) -g -lm -DMOUSE_MOVEMENT -DWIDTH=1280 -DHEIGHT=720 $@
